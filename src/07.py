1f77688c99293e8ad2bad3bf5d846c0e8d69f263
Bastien Laby <bastien.laby@gmail.com>
2020-12-08 23:17:49 +0100

    fix day07

--- src/07.py
+++ src/07.py
@@ -3,7 +3,7 @@ from parse import parse
 from utils import read_input
 
 
-_rule_pattern = "{src} contain {dst}"
+_rule_pattern = "{src} bags contain {dst}"
 
 
 def parse_recipe(line):
@@ -15,13 +15,55 @@ def parse_recipe(line):
     res = parse(_rule_pattern, line)
     src = res["src"]
     if "no other bag" in res["dst"]:
-        return (src, None)
+        return (src, [])
     comps = []
     for comp in res["dst"].split(","):
-        # print(comp)
         count, elt = comp.rstrip(".").rstrip("bags").rstrip("bag").strip().split(" ", 1)
         comps.append((elt, int(count)))
     return (src, comps)
+
+
+_bags = {}
+
+
+def contains_bag(bag, other_bag):
+    """Returns True if bag can contains another bag, else returns False.
+    :str bag: name of the cotainer bag
+    :str other_bag: name of the content bag to check
+    """
+    bag_content = [
+        comp[0]
+        for comp in _bags[bag]
+    ]
+    if other_bag in bag_content:
+        return True
+    else:
+        return any(
+            contains_bag(child_bag, other_bag)
+            for child_bag in bag_content
+        )
+
+
+def part01():
+    return len([
+        None
+        for bag in _bags
+        if contains_bag(bag, "shiny gold")
+    ])
+
+
+def count_bags(bag):
+    bag_content =  _bags[bag]
+    if not bag_content:
+        return 1  # this bag
+    total = 1
+    for other_bag, count in bag_content:
+        total += count * count_bags(other_bag)
+    return total
+
+
+def part02():
+    return count_bags("shiny gold") - 1  # shiny gold doesnt count
 
 
 if __name__ == "__main__":
@@ -32,4 +74,8 @@ if __name__ == "__main__":
     ]
 
     for rule in input:
-        print(parse_recipe(rule))
+        src, comps = parse_recipe(rule)
+        _bags[src] = comps
+
+    print(part01())
+    print(part02())
